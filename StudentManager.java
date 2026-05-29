07732c6551ee1433ca6694dd3df1bf02d203f9f4
Ofijan Endale <ofijan.endale@bitscollege.edu.et>
2026-05-29 17:22:34 +0300

    Remove searchStudent method from StudentManager
    Removed the searchStudent method for finding students by ID.

--- StudentManager.java
+++ StudentManager.java
@@ -28,22 +28,4 @@ public class StudentManager {
             student.displayInfo();
         }
     }
-
-    // Search student by ID
-    public Student searchStudent(String id) {
-
-        // Loop through all students
-        for(Student student : students) {
-
-            // Compare IDs
-            if(student.getStudentId().equalsIgnoreCase(id)) {
-
-                // Return matching student
-                return student;
-            }
-        }
-
-        // If no student found
-        return null;
-    }
 }
